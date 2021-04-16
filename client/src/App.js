5b3bbdd50c0051232c753a96d3ed0f82b9cc7a28
koashima <48111257+koashima@users.noreply.github.com>
2021-04-16 00:06:08 -0500

    set up basic client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Squawk!</h1>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <div className="App">
+        <header className="App-header">
+          <h1>Squawk!</h1>
+        </header>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
