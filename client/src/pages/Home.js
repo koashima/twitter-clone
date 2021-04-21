0a51e2a90cb5630ceb3f7edb12064aa1d806a785
koashima <48111257+koashima@users.noreply.github.com>
2021-04-20 22:21:36 -0700

    Fix fetching bug with conditional loading and Add styling to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,7 @@ import gql from 'graphql-tag';
 
 const Home = () => {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-  if (data) {
-    console.log(data);
-  }
+
   return (
     <>
       <header className="bg-white shadow">
@@ -15,42 +13,50 @@ const Home = () => {
         </div>
       </header>
       <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {data.getPosts.map((post) => (
-            <div className="bg-gray-50 dark:bg-black p-10 flex items-center justify-center">
-              <div className="bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 p-4 rounded-xl border max-w-xl">
-                <div className="flex justify-between">
-                  <div className="flex items-center">
-                    <img
-                      className="h-11 w-11 rounded-full"
-                      src="https://pbs.twimg.com/profile_images/1287562748562309122/4RLk5A_U_x96.jpg"
-                    />
-                    <div className="ml-1.5 text-sm leading-tight">
-                      <span className="text-black dark:text-white font-bold block ">
-                        {post.username}
-                      </span>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex justify-center">
+          <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+            {loading ? (
+              <div>loading...</div>
+            ) : (
+              data.getPosts &&
+              data.getPosts.map((post) => (
+                <div
+                  className="bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 p-4 rounded-xl border max-w-xl mb-2"
+                  key={post.id}
+                >
+                  <div className="flex justify-between">
+                    <div className="flex items-center">
+                      <img
+                        className="h-11 w-11 rounded-full"
+                        src="https://pbs.twimg.com/profile_images/1287562748562309122/4RLk5A_U_x96.jpg"
+                      />
+                      <div className="ml-1.5 text-sm leading-tight">
+                        <span className="text-black dark:text-white font-bold block ">
+                          {post.username}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="text-black dark:text-white block text-xl leading-snug mt-3">
+                    {post.body}
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400 text-xs py-1 my-0.5">
+                    {post.createdAt}
+                  </p>
+
+                  <hr />
+                  <div className="text-gray-500 dark:text-gray-400 flex justify-between mt-3">
+                    <div className="mr-6">
+                      <span className="ml-3">likes:{post.likeCount}</span>
+                    </div>
+                    <div className=" mr-6">
+                      <span className="ml-3">comments:{post.commentCount}</span>
                     </div>
                   </div>
                 </div>
-                <p className="text-black dark:text-white block text-xl leading-snug mt-3">
-                  {post.body}
-                </p>
-                <p className="text-gray-500 dark:text-gray-400 text-xs py-1 my-0.5">
-                  {post.createdAt}
-                </p>
-
-                <hr />
-                <div className="text-gray-500 dark:text-gray-400 flex mt-3">
-                  <div className="flex items-center mr-6">
-                    <span className="ml-3">likes:{post.likeCount}</span>
-                  </div>
-                  <div className="flex justify-self-end mr-6">
-                    <span className="ml-3">comments:{post.commentCount}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))
+            )}
+          </div>
         </div>
       </main>
     </>
